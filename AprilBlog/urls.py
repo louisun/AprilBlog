6ef36158f7c80a0afeb869ba8f69db4ff26c452b
louisun <read_on@126.com>
2016-05-08 17:55:59 +0800

    add githook

--- AprilBlog/urls.py
+++ AprilBlog/urls.py
@@ -4,12 +4,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from blog.views import BlogListView
+from AprilBlog import views
 
 urlpatterns = [
 
                   url(r'^admin/', admin.site.urls),
                   url(r'^blog/', include('blog.urls', namespace='blog')),
                   url(r'^$', BlogListView.as_view(), name='index'),
+                  url(r'^githook/', views.githook),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
