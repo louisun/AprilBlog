1d5704f4c64591df523b07c3f461997e89f551a0
louisun <read_on@126.com>
2016-05-15 23:29:35 +0800

    fix githook

--- AprilBlog/views.py
+++ AprilBlog/views.py
@@ -36,7 +36,7 @@ def githook(request):
 
     try:
         # check sha1 signature
-        name, req_sha1 = request.META.get('HTTP_X_HUB_SIGNATURE', '').split('=')
+        name, req_sha1 = request.META.get('X_HUB_SIGNATURE', '').split('=')
         if name != 'sha1':
             return HttpResponse('Signature format error')
         HOOK_SECRET_KEY = get_object_or_404(GithubHookSecret, pk=1)
